e3bd61be62332f7f82f7b23a391b846f91e3a969
ohnas <ohnas12@gmail.com>
2024-01-04 18:21:39 +0900

    get_transcript

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from youtube_transcript_api import YouTubeTranscriptApi
 
-captions = YouTubeTranscriptApi.get_transcript("2lAe1cqCOXo", languages=["en", "ko"])
+captions = YouTubeTranscriptApi.get_transcript("p64_MLw-ios", languages=["ko"])
 
 for caption in captions:
     print(caption)
